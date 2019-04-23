869270f3b7d11c279bdf2024b6ea79c6bb4d3a4c
Russia9 <a.kovalev9@hotmail.com>
2019-04-23 07:23:29 +0300

    Join and quit messages

--- src/main/java/ru/epicserver/chatnukkit/ChatNukkit.java
+++ src/main/java/ru/epicserver/chatnukkit/ChatNukkit.java
@@ -5,15 +5,15 @@ import cn.nukkit.command.CommandSender;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerChatEvent;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
 import cn.nukkit.math.Vector2;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 import com.creeperface.nukkit.placeholderapi.api.PlaceholderAPI;
 import me.lucko.luckperms.LuckPerms;
-import me.lucko.luckperms.api.Contexts;
 import me.lucko.luckperms.api.LuckPermsApi;
 import me.lucko.luckperms.api.User;
-import me.lucko.luckperms.api.caching.MetaData;
 import ru.epicserver.chatnukkit.lib.Helper;
 
 import java.util.HashSet;
@@ -22,8 +22,8 @@ import java.util.Set;
 import java.util.UUID;
 
 public class ChatNukkit extends PluginBase implements Listener {
-    public static LuckPermsApi luckPerms = null;
-    public static PlaceholderAPI placeholderApi = null;
+    private LuckPermsApi luckPerms;
+    private PlaceholderAPI placeholderApi;
 
     public void onEnable() {
         this.saveDefaultConfig();
@@ -39,11 +39,9 @@ public class ChatNukkit extends PluginBase implements Listener {
             return;
         }
 
-        // Check for Placeholder API
         try {
             placeholderApi = PlaceholderAPI.getInstance();
-        } catch (Throwable e) {
-            // ignore
+        } catch (Throwable ignored) {
         }
 
 
@@ -52,41 +50,32 @@ public class ChatNukkit extends PluginBase implements Listener {
     }
 
     @EventHandler
-    public void onChat(PlayerChatEvent e) {
-        Player p = e.getPlayer();
-        String name = p.getDisplayName();
-        String message = e.getMessage();
-        User user = luckPerms.getUser(p.getUniqueId());
+    public void onChat(PlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+        User user = luckPerms.getUser(player.getUniqueId());
         if (user == null) {
-            this.getLogger().warning("An error occurred when attempting to retrieve " + p.getName() + "'s user data!");
+            this.getLogger().warning("An error occurred when attempting to retrieve " + player.getName() + "'s user data!");
             return;
         }
-        Contexts contexts = luckPerms.getContextManager().getApplicableContexts(p);
-        MetaData metaData = user.getCachedData().getMetaData(contexts);
-
-        String prefix = metaData.getPrefix();
-        prefix = prefix != null ? prefix : "Игрок";
+        String nick = Helper.getNick(player, this);
         String msg;
         message = message.replace("§", "&");
         String perm = user.getPrimaryGroup();
         if (message.startsWith("!")) { // Global
             message = message.substring(1);
-            msg = (" §7[§2G§7] §r§7[§f%prefix%§7] §r%name%§r: %msg%"
-                    .replace("%name%", p.getName())
-                    .replace("%disname%", name)
-                    .replace("%prefix%", prefix)
+            msg = (" §7[§2G§7] §r%nick%§r: %msg%"
+                    .replace("%nick%", nick)
                     .replace("%group%", perm)
                     .replace("%msg%", message));
         } else {
-            msg = (" §7[L] §r§7[§f%prefix%§7] §r%name%§r: %msg%"
-                    .replace("%name%", p.getName())
-                    .replace("%disname%", name)
-                    .replace("%prefix%", prefix)
+            msg = (" §7[L] §r%nick%§r: %msg%"
+                    .replace("%nick%", nick)
                     .replace("%group%", perm)
                     .replace("%msg%", message));
             Map<UUID, Player> players = getServer().getOnlinePlayers();
             Set<CommandSender> receivers = new HashSet<>();
-            Vector2 posPlayer = new Vector2(p.getX(), p.getZ());
+            Vector2 posPlayer = new Vector2(player.getX(), player.getZ());
             for (Map.Entry<UUID, Player> entry : players.entrySet()) {
                 Vector2 posReceiver = new Vector2(entry.getValue().getX(), entry.getValue().getZ());
                 if (posPlayer.distance(posReceiver) <= 100) {
@@ -94,15 +83,44 @@ public class ChatNukkit extends PluginBase implements Listener {
                 }
             }
             receivers.add(getServer().getConsoleSender());
-            e.setRecipients(receivers);
+            event.setRecipients(receivers);
         }
         if (placeholderApi != null) {
-            msg = placeholderApi.translateString(msg, p);
+            msg = placeholderApi.translateString(msg, player);
         }
-        if(p.hasPermission("chatnukkit.chatformat")) {
-            e.setFormat(TextFormat.colorize('&', msg));
+        if (player.hasPermission("chatnukkit.chatformat")) {
+            event.setFormat(TextFormat.colorize('&', msg));
         } else {
-            e.setFormat(TextFormat.colorize('§', msg));
+            event.setFormat(TextFormat.colorize('§', msg));
         }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String nick = Helper.getNick(player, this);
+        String msg = " %nick%§e присоединился!";
+        if (!player.playedBefore) {
+            msg = " %nick%§e впервые зашел на сервер!";
+        }
+        msg = msg.replace("%nick%", nick).replace("&", "§");
+        event.setJoinMessage(msg);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        String nick = Helper.getNick(player, this);
+        String msg = " %nick%§e вышел!";
+        msg = msg.replace("%nick%", nick).replace("&", "§");
+        event.setQuitMessage(msg);
+    }
+
+    public LuckPermsApi getLuckPerms() {
+        return luckPerms;
+    }
+
+    public PlaceholderAPI getPlaceholderApi() {
+        return placeholderApi;
     }
 }
